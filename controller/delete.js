7de6c707b5cf4bf9efe269c163b30c66e0104b97
Avoler0 <jys1391@gmail.com>
2023-11-03 17:11:29 +0900

    Fix: 삭제 부분 오류 수정

--- controller/delete.js
+++ controller/delete.js
@@ -2,19 +2,21 @@ import products from "../schemas/products.schema.js";
 
 export const deleteProduct = async (req,res) => {
   const { productId } = req.params;
+  const { password } = req.body;
+
+  if(!Object.keys(req.body).length) return res.status(400).send({ message:'데이터 형식이 올바르지 않습니다.' })
 
   try{
     const checkResult = await products.find({_id:productId})
 
     if(!checkResult.length) return res.status(404).send({ message:'상품 조회에 실패하였습니다.' })
     if(checkResult[0].password !== password) return res.status(401).send({ message:'상품을 수정할 권한이 존재하지 않습니다.' })
-    
+
     await products.deleteOne({_id:productId});
 
     res.status(200).send({message:"상품을 삭제하였습니다."})
   }catch(err){
-    if(err.name === "CastError") return res.status(400).send({ message:'데이터 형식이 올바르지 않습니다.' })
-
-    res.status(404).send(err)
+    console.log('삭제 에러',err)
+    return res.status(400).send({ message:'데이터 형식이 올바르지 않습니다.' ,err})
   }
 }
