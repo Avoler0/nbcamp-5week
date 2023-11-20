56b0f40602300d8bd411cc687df00710952255ba
Avoler0 <jys1391@gmail.com>
2023-11-20 11:29:25 +0900

    Fix : models column 수정

--- src/models/users.js
+++ src/models/users.js
@@ -9,16 +9,28 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Users.init({
-    id: {
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    email:{
-      unique:true,
-      type: DataTypes.STRING,
-    },
-    password: DataTypes.STRING
-  }, {
+    user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      email: {
+        unique:true,
+        type: DataTypes.STRING
+      },
+      password: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    }, {
     sequelize,
     modelName: 'Users',
   });
