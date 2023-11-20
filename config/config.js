38cc144c2a611fdd3a2f54d1aa93b4abc4b82edb
Avoler0 <jys1391@gmail.com>
2023-11-20 11:15:49 +0900

    Fix : sql 연결 에러 수정

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_ID,
-    "password": process.env.DB_PW,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_URL,
+    "username": process.env.DB_DEVELOPMENT_ID,
+    "password": process.env.DB_DEVELOPMENT_PW,
+    "database": process.env.DB_DEVELOPMENT_NAME,
+    "host": process.env.DB_DEVELOPMENT_URL,
     "dialect": "mysql"
   },
   "test": {
@@ -16,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_ID,
-    "password": process.env.DB_PW,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_URL,
+    "username": process.env.DB_PRODUCTION_ID,
+    "password": process.env.DB_PRODUCTION_PW,
+    "database": process.env.DB_PRODUCTION_NAME,
+    "host": process.env.DB_PRODUCTION_URL,
     "dialect": "mysql"
   }
 }
